0d1c9331a2fc753b0e48e584519367974f49fc7c
PhuscBui <duyphucbui522@gmail.com>
2024-10-19 20:01:48 +0700

    Minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import { ModeToggle } from "./components/mode-toggle";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="">
       <ModeToggle />
+      <Button>Click me</Button>
     </div>
   );
 }
