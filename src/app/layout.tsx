7915a4583f5a88b8d4cc23c427a54f0e0346d001
NTDKhoa04 <146697000+NTDKhoa04@users.noreply.github.com>
2024-10-22 10:25:13 +0700

    Minor change to colors configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ThemeProvider } from "./components/theme-provider";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import Header from "@/components/header";
